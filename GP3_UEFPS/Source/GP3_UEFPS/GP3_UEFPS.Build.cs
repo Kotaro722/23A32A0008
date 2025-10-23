54c2717c973097f65fd2f20c1f425d8f4cc722a2
HasuiTakumu <ktc25a31a0045@edu.kyoto-tech.ac.jp>
2025-10-23 13:54:19 +0900

    2025/10/23

---
+++ GP3_UEFPS/Source/GP3_UEFPS/GP3_UEFPS.Build.cs
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class GP3_UEFPS : ModuleRules
+{
+	public GP3_UEFPS(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+		PublicDependencyModuleNames.AddRange(new string[] {
+			"Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput",
+            "OnlineSubsystem","OnlineSubsystemUtils"
+        });
+	}
+}
---
+++ GP3_UEFPS/Source/GP3_UEFPS/Public/SessionSubsystem.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Subsystems/GameInstanceSubsystem.h"
+#include "SessionSubsystem.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class GP3_UEFPS_API USessionSubsystem : public UGameInstanceSubsystem
+{
+	GENERATED_BODY()
+	
+};
